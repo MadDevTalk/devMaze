370027a0d5ff001922ef7b94f5d1bd8b837a0f36
mwiemer <mwiemer@outlook.com>
2013-10-04 21:03:00 -0500

    GameFrame 1.0

---
+++ devMaze/src/dev/talk/maze/GameFrame.java
@@ -0,0 +1,44 @@
+package dev.talk.maze;
+
+public class GameFrame {
+	/* This is a gaming outline that covers the six major notifications that
+	 * Android sends an application. We can pretty much ignore Pause, Resume,
+	 * and Resize until we target Android. 
+	 */
+	
+	// Runs when the application is first instantiated
+	public void create() {
+	/* Load the Images and the sounds
+	 * Create the Camera object and position it at the center of the screen 
+	 * Create the SpriteBatch object
+	 * Set the screen to the MenuScreen
+	 */
+		
+	}
+	
+	// Save user app information
+	public void pause() {
+		
+	}
+	
+	// Return from pause
+	public void resume() {
+		
+	}
+	
+	// The main loop, fires up to 60 fps 
+	// LibGDX combines the main and user input threads
+	public void render() {
+		
+	}
+	
+	// Resizes
+	public void resize() {
+		
+	}
+	
+	// Kills the app. Calls a pause first
+	public void dispose() {
+		
+	}
+}
