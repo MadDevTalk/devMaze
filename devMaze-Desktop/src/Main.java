7b71a167bd4dea953b535a1f20cd9ec17fe3b7bb
eggers <eggers.max@gmail.com>
2013-10-04 23:06:27 -0500

    added desktop starter class; not sure about the package vs. import thing for dev.talk.maze

---
+++ devMaze-Desktop/src/Main.java
@@ -0,0 +1,11 @@
+import dev.talk.maze.GameFrame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+   public static void main(String[] args) {
+      LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+      new LwjglApplication(new GameFrame(), cfg);
+   }
+}
