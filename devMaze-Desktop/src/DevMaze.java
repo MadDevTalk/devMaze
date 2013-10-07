a3dddedcc116e7488a39b998f300b92fe17a353c
mwiemer <mwiemer@outlook.com>
2013-10-07 13:02:11 -0500

    Change main file name and set default viewing
    I chose 1243 x 768 because it's a golden rectangle

--- devMaze-Desktop/src/DevMaze.java
+++ devMaze-Desktop/src/DevMaze.java
@@ -3,9 +3,12 @@ import dev.talk.maze.GameFrame;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
+public class DevMaze {
    public static void main(String[] args) {
       LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+      cfg.width = 1243;
+      cfg.height = 768;
+      
       new LwjglApplication(new GameFrame(), cfg);
    }
 }
