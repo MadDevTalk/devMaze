c5e370f57d17f5ad811d95b5fae98c3e1ae877df
mwiemer <mwiemer@outlook.com>
2013-10-17 22:42:38 -0500

    Screens on screens
    Added a loading screen and a region that can be tapped to pause. Got me
    an awesome Screen in the works

--- DevMaze-desktop/src/com/devtalk/maze/DesktopStarter.java
+++ DevMaze-desktop/src/com/devtalk/maze/DesktopStarter.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 public class DesktopStarter {
 	public static void main(String[] args) throws IOException {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		TexturePacker2.process("img", "../DevMaze-android/assets/", "starters.pak");
+		// TexturePacker2.process("img", "../DevMaze-android/assets/", "starters.pak");
 		cfg.title = "DevMaze";
 		cfg.useGL20 = false;
 		cfg.width = 800;
--- DevMaze/src/com/devtalk/maze/LoadingScreen.java
+++ DevMaze/src/com/devtalk/maze/LoadingScreen.java
@@ -4,19 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-public class PauseScreen implements Screen {
-	
+public class LoadingScreen implements Screen {
 	final DevMaze game;
 	OrthographicCamera camera;
-	TextureAtlas charsheet;
-	GameScreen gamestate;
+	int i = 0;
 	
-	public PauseScreen(final DevMaze game, GameScreen gamestate) {
+	public LoadingScreen(final DevMaze game) {
 		this.game = game;
-		this.gamestate = gamestate;
-		
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 	}
@@ -28,48 +23,50 @@ public class PauseScreen implements Screen {
 		
 		game.batch.setProjectionMatrix(camera.combined);
 		game.batch.begin();
-		game.font.draw(game.batch, "Resume", 100, 150);
+		game.font.draw(game.batch, "Loading...", 100, 150);
 		game.batch.end();
-	
-		if (Gdx.input.justTouched()) {
-			game.setScreen(gamestate);
+		
+		if (i > 5) {
+			game.setScreen(new MainMenuScreen(game));
 			this.dispose();
 		}
+		i++;
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 }
