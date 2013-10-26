8944b2fa343863f54b9a77474c01dea83fe26951
mwiemer <mwiemer@outlook.com>
2013-10-26 18:21:11 -0500

    Got the a working plus sign
    Now its time to put the buttons in their place

--- DevMaze/src/com/devtalk/maze/MainMenuScreen.java
+++ DevMaze/src/com/devtalk/maze/MainMenuScreen.java
@@ -10,14 +10,18 @@ import com.badlogic.gdx.math.Vector3;
 
 public class MainMenuScreen implements Screen {
 
-	final DevMaze game;
 	Tile[][] paths;
 	GameScreen gamestate;
 	OrthographicCamera camera;
 	Texture gameImg, resumeImg, playerImg, settingsImg, quitImg;
 	Rectangle newGame, resume, player, settings, quit;
+	
+	final DevMaze game;
+	private static Texture IN_MAZE;
+	public static final int camCenter = (5 * GameScreen.EDGE_SIZE_PX)/2;
+	
 	int i, x, y;
-
+	
 	public MainMenuScreen(final DevMaze game, GameScreen gamestate) {
 		this.game = game;
 		if(gamestate != null) {
@@ -33,9 +37,10 @@ public class MainMenuScreen implements Screen {
 		playerImg = new Texture(Gdx.files.internal("PLAYER.png"));
 		settingsImg = new Texture(Gdx.files.internal("SETTINGS.png"));
 		quitImg = new Texture(Gdx.files.internal("QUIT.png"));
+		IN_MAZE = new Texture(Gdx.files.internal("IN_MAZE.png"));
 		
 		// Make Maze
-		paths = new Tile[5][10];
+		paths = new Tile[5][5];
 		
 		// Place the buttons
 		x = 800 - 100 - 128;
@@ -49,12 +54,13 @@ public class MainMenuScreen implements Screen {
 		player = new Rectangle(x, y, 192, 64);
 		y = 395;
 		quit = new Rectangle(x, y, 192, 64);
-		
+
 	}
 
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);   // R,G,B,A (0.0f - 1.0f)
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		camera.position.set(camCenter, camCenter, 0);
 		camera.update();
 
 		game.batch.setProjectionMatrix(camera.combined);
@@ -62,39 +68,31 @@ public class MainMenuScreen implements Screen {
 		game.font.draw(game.batch, "Welcome to DevMaze! ", 100, 150);
 		
 		// **DRAW BUTTONS** //
-		game.batch.draw(gameImg, newGame.x, newGame.y);   // Double draw is a hack 
-		game.batch.draw(gameImg, newGame.x, newGame.y);   // around the ^2 rule
+		game.batch.draw(gameImg, newGame.x, newGame.y);
 		game.font.draw(game.batch, "NEW GAME", newGame.x + 25, newGame.y + 25);
 		
-		game.batch.draw(resumeImg, resume.x, resume.y);   // Double draw is a hack 
-		game.batch.draw(resumeImg, resume.x, resume.y);   // around the ^2 rule
+		game.batch.draw(resumeImg, resume.x, resume.y);
 		game.font.draw(game.batch, "RESUME", resume.x + 25, resume.y + 25);
 		
-		game.batch.draw(playerImg, player.x, player.y);   // Double draw is a hack 
-		game.batch.draw(playerImg, player.x, player.y);   // around the ^2 rule
+		game.batch.draw(playerImg, player.x, player.y);
 		game.font.draw(game.batch, "PLAYER", player.x + 25, player.y + 25);
 		
-		game.batch.draw(settingsImg, settings.x, settings.y);   // Double draw is a hack 
-		game.batch.draw(settingsImg, settings.x, settings.y);   // around the ^2 rule
+		game.batch.draw(settingsImg, settings.x, settings.y);
 		game.font.draw(game.batch, "SETTINGS", settings.x + 25, settings.y + 25);
 		
-		game.batch.draw(quitImg, quit.x, quit.y);   // Double draw is a hack 
-		game.batch.draw(quitImg, quit.x, quit.y);   // around the ^2 rule
+		game.batch.draw(quitImg, quit.x, quit.y);
 		game.font.draw(game.batch, "QUIT", quit.x + 25, quit.y + 25);
 		
 		// **DRAW SYMBOL** //
 		for (int i = 0; i < paths.length; i++) {
 			for (int j = 0; j < paths[0].length; j++) {
 				
-				if(i = 5 || j == 5) {
-					game.batch.draw(region, j, j, originX, originY, width, height, scaleX, scaleY, rotation)
-					game.batch.draw(region, j, j, originX, originY, width, height, scaleX, scaleY, rotation)
+				if(i == 2 || j == 2) {
+					game.batch.draw(IN_MAZE, j * GameScreen.EDGE_SIZE_PX, i * GameScreen.EDGE_SIZE_PX);
 				}
 				
-				
-				
 				// Wanna see the indices overlaid on the maze? Uncomment this line right here
-				// game.font.draw(game.batch, maze.tiles[i][j].toString(), j * EDGE_SIZE_PX + 15, i * EDGE_SIZE_PX + 40);
+				// game.font.draw(game.batch, maze.tiles[i][j].toString(), j * GameScreen.EDGE_SIZE_PX + 15, i * GameScreen.EDGE_SIZE_PX + 40);
 			}
 	}
 
