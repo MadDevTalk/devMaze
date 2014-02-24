c9ad46bdb9c2547f8a1549525b11af9b886ee80c
unclegeoff <hagoogablob@Geoffery-Gilless-MacBook-Pro.local>
2014-02-23 22:25:45 -0600

    Actually fixed main class issue

--- devMaze-robovm/src/com/devTalk/devMaze/RobovmLauncher.java
+++ devMaze-robovm/src/com/devTalk/devMaze/RobovmLauncher.java
@@ -5,8 +5,8 @@ import org.robovm.cocoatouch.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.me.mygdxgame.MyGdxGame;
-import com.me.mygdxgame.RobovmLauncher;
+import com.devTalk.devMaze.maze.DevMaze;
+import com.devTalk.devMaze.RobovmLauncher;
 
 public class RobovmLauncher extends IOSApplication.Delegate {
 	@Override
@@ -14,7 +14,7 @@ public class RobovmLauncher extends IOSApplication.Delegate {
 		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
 		config.orientationLandscape = true;
 		config.orientationPortrait = false;
-		return new IOSApplication(new MyGdxGame(), config);
+		return new IOSApplication(new DevMaze(), config);
 	}
 
 	public static void main(String[] argv) {
